803dd78d428b4b7dddce982e9b3a96f9f5f8846d
sosaeio <ekhoragbonesosa1@gmail.com>
2023-10-30 08:42:15 -0700

    Post Merge scripts

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-The 'run.sh' bash scripts installs a github cli tool on your Mac or Linux machine
+These bash scripts installs a github cli tool on your Mac or Linux machine
 
-change the run.sh file to an executable using 'chmod +x ./run.sh' command
+Run the following command in your terminal
+`bash ghcli-$(uname | tr '[:upper:]' '[:lower:]').sh`
+
--- ghcli-linux.sh
+++ ghcli-linux.sh
@@ -4,10 +4,10 @@
 # Date: Oct 28, 2023
 ####################################
 
-if [ $(which gh | grep -o gh > /dev/null && echo 1 || echo 0) -eq 1 ]; then
+if ( which gh ); then
         echo -e '\033[0;32m' $(perl -e 'print "\xE2\x9C\x94"') Github cli tool already installed in $(whereis gh | awk '{ print $2 }') ... '\033[0m'
 else
         echo -e '\033[0;33m' Installing Github cli tool ... '\033[0m'
-	sudo apt install gh
+	sudo apt install -y gh
         echo -e '\033[0;32m' $(perl -e 'print "\xE2\x9C\x94"') Installation Completed '\033[0m'
 fi
